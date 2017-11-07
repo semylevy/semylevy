6ac4cbcbaec5c0c9ff38eda1e22f6d16b6ba2658
Semy Levy <semylevy@hotmail.com>
2017-11-07 00:45:20 -0600

    New UI, transformations

--- js/transformaciones.js
+++ js/transformaciones.js
@@ -1,18 +1,41 @@
-var canvas, width, q, coord, w_size;
+var canvas, q, coord, w_size, shape_array, curr_coords;
 
 function setup () {
-  w_size = 600;
+  curr_coords = [0, 0];
+  w_size = document.getElementById('t_space').offsetWidth;
   canvas = createCanvas(w_size, w_size);
   canvas.parent('t_space');
   q = new Queue;
-  s = new Shape;
-  s.polygon(8, 50, 50, 0);
-  s.reflect(0);
-  s.reflect(1);
-  //s.translate(20, 5);
-  //s.rotate(180, 50, 50);
-  //s.scale(1.2, 1.2);
-  s.print();
+  shape_array = [];
+}
+
+function newShape(type) {
+  var s = new Shape;
+  var x1, y1, length, sides, tx, ty, angle, rx, ry, sx, sy;
+  x1 = curr_coords[0];
+  y1 = curr_coords[1];
+  sides = parseInt(document.getElementById('side_num').value);
+  length = parseInt(document.getElementById('side_length').value);
+  tx = parseInt(document.getElementById('translate_val_x').value);
+  ty = parseInt(document.getElementById('translate_val_y').value);
+  angle = parseInt(document.getElementById('rotate_val_theta').value);
+  rx = parseInt(document.getElementById('rotate_val_x').value);
+  ry = parseInt(document.getElementById('rotate_val_y').value);
+  sx = parseInt(document.getElementById('scale_val_x').value);
+  sy = parseInt(document.getElementById('scale_val_y').value);
+
+  switch (type) {
+    case 0:
+      s.polygon(sides, length, x1, y1);
+      s.translate(tx, ty);
+      s.rotate(angle, rx, ry);
+      //s.scale(sx, sy);
+      s.print();
+      shape_array.push(s);
+      break;
+    default:
+      break;
+  }
 }
 
 function draw () {
@@ -56,13 +79,14 @@ class Shape {
   print() {
     for(var i = 0; i < this.coords.length; i++) {
       q.push([this.coords[i][0], this.coords[i][1]]);
-      console.log(this.coords[i][0]);
     }
   }
   translate(tx, ty) {
     for(var i = 0; i < this.coords.length; i++) {
+      console.log(this.coords[i][0]);
       this.coords[i][0] += tx;
       this.coords[i][1] += ty;
+      console.log(this.coords[i][0]);
     }
   }
 
@@ -139,7 +163,6 @@ class Shape {
       inverted *= -1;
     }
   }
-
 }
 
 function drawAxis() {
@@ -147,4 +170,36 @@ function drawAxis() {
   line(0, w_size*0.5, w_size, w_size*0.5);
   line(w_size*0.5, 0, w_size*0.5, w_size);
   stroke(0,0,0);
+}
+
+function setListener() {
+  document.getElementById("t_space").addEventListener("click", getCoords);
+  document.getElementById("t_space").addEventListener("mousemove", printCoords);
+  document.getElementById("t_space").style.cursor = "crosshair";
+}
+
+function getCoords() {
+  var x = event.clientX;     // Get the horizontal coordinate
+  var y = event.clientY;     // Get the vertical coordinate
+  if(x < w_size && y < w_size) {
+    var t_space = document.getElementById("t_space");
+    var tooltipSpan = document.getElementById('tooltip-span');
+    t_space.removeEventListener("click", getCoords);
+    t_space.removeEventListener("mousemove", printCoords);
+    t_space.style.cursor = "default";
+    tooltipSpan.innerHTML = "";
+    curr_coords[0] = x - 16 - w_size/2;
+    curr_coords[1] = y - w_size/2;
+  }
+}
+
+function printCoords() {
+  var tooltipSpan = document.getElementById('tooltip-span');
+  var coords_text = document.getElementById('coords_text');
+  var x = event.clientX;
+  var y = event.clientY;
+  tooltipSpan.style.top = (y + 20) + 'px';
+  tooltipSpan.style.left = (x + 20) + 'px';
+  tooltipSpan.innerHTML = Math.round(x - 16 - w_size/2) + ', ' + Math.round(y - w_size/2);
+  coords_text.innerHTML = Math.round(x - 16 - w_size/2) + ', ' + Math.round(y - w_size/2);
 }
