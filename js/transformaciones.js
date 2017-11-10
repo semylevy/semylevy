29ccd5b915206cd2227497da60817decff39a6d2
Semy Levy <semylevy@hotmail.com>
2017-11-10 11:20:07 -0600

    Added more transformations, 3D shapes, arcs and visual fixes

--- js/transformaciones.js
+++ js/transformaciones.js
@@ -1,41 +1,14 @@
-var canvas, q, coord, w_size, shape_array, curr_coords;
+var canvas, type, q, coord, shape_array, curr_coords, w_height, w_width;
 
 function setup () {
   curr_coords = [0, 0];
-  w_size = document.getElementById('t_space').offsetWidth;
-  canvas = createCanvas(w_size, w_size);
+  w_height = document.documentElement.clientHeight;
+  w_width = document.getElementById('t_space').offsetWidth;
+  canvas = createCanvas(w_width, w_height);
   canvas.parent('t_space');
   q = new Queue;
   shape_array = [];
-}
-
-function newShape(type) {
-  var s = new Shape;
-  var x1, y1, length, sides, tx, ty, angle, rx, ry, sx, sy;
-  x1 = curr_coords[0];
-  y1 = curr_coords[1];
-  sides = parseInt(document.getElementById('side_num').value);
-  length = parseInt(document.getElementById('side_length').value);
-  tx = parseInt(document.getElementById('translate_val_x').value);
-  ty = parseInt(document.getElementById('translate_val_y').value);
-  angle = parseInt(document.getElementById('rotate_val_theta').value);
-  rx = parseInt(document.getElementById('rotate_val_x').value);
-  ry = parseInt(document.getElementById('rotate_val_y').value);
-  sx = parseFloat(document.getElementById('scale_val_x').value);
-  sy = parseFloat(document.getElementById('scale_val_y').value);
-
-  switch (type) {
-    case 0:
-      s.polygon(sides, length, x1, y1);
-      s.translate(tx, ty);
-      s.rotate(angle, rx, ry);
-      s.scale(sx, sy);
-      s.print();
-      shape_array.push(s);
-      break;
-    default:
-      break;
-  }
+  type = 0;
 }
 
 function draw () {
@@ -44,6 +17,113 @@ function draw () {
     coord = q.peek();
     point(coord[0], coord[1]);
     q.pop();
+  }
+}
+
+$(document).ready(function(){
+  $('.nav-tabs a[href="#polygon"]').on('shown.bs.tab', function(){
+    type = 0;
+  });
+  $('.nav-tabs a[href="#arc_shapes"]').on('shown.bs.tab', function(){
+    type = 1;
+  });
+  $('.nav-tabs a[href="#3d-cube"]').on('shown.bs.tab', function(){
+    type = 2;
+  });
+  $('.nav-tabs a[href="#3d-prism-rectangular"]').on('shown.bs.tab', function(){
+    type = 3;
+  });
+  $('.nav-tabs a[href="#3d-prism-triangular"]').on('shown.bs.tab', function(){
+    type = 4;
+  });
+  $('.nav-tabs a[href="#3d-cone"]').on('shown.bs.tab', function(){
+    type = 5;
+  });
+});
+
+function newShape() {
+  var s = new Shape;
+  var x1, y1, length, sides, tx, ty, angle, rx, ry, sx, sy, rf_x, rf_y, rf_p;
+  x1 = curr_coords[0];
+  y1 = curr_coords[1];
+  tx = parseInt(document.getElementById('translate_val_x').value);
+  ty = parseInt(document.getElementById('translate_val_y').value);
+  angle = parseInt(document.getElementById('rotate_val_theta').value);
+  rx = parseInt(document.getElementById('rotate_val_x').value);
+  ry = parseInt(document.getElementById('rotate_val_y').value);
+  sx = parseFloat(document.getElementById('scale_val_x').value);
+  sy = parseFloat(document.getElementById('scale_val_y').value);
+  rf_x = document.getElementById('reflect_x').checked;
+  rf_y = document.getElementById('reflect_y').checked;
+  rf_p = document.getElementById('reflect_parallel').checked;
+
+  switch (type) {
+    case 0:
+      length = parseInt(document.getElementById('polygon_side_length').value);
+      sides = parseInt(document.getElementById('side_num').value);
+      s.polygon(sides, length, x1, y1);
+      s.translate(tx, ty);
+      s.rotate(angle, rx, ry);
+      s.scale(sx, sy);
+      if(rf_x) s.reflect(1);
+      if(rf_y) s.reflect(2);
+      if(rf_p) s.reflect(3);
+      s.print();
+      shape_array.push(s);
+      break;
+    case 1:
+      var radius = parseInt(document.getElementById('arc_radius').value);
+      s.circle(x1, y1, radius);
+      s.translate(tx, ty);
+      s.rotate(angle, rx, ry);
+      s.scale(sx, sy);
+      if(rf_x) s.reflect(1);
+      if(rf_y) s.reflect(2);
+      if(rf_p) s.reflect(3);
+      s.print();
+      shape_array.push(s);
+      break;
+    case 2:
+      length = parseInt(document.getElementById('cube_side_length').value);
+      s.cube(length, x1, y1);
+      s.translate(tx, ty);
+      s.rotate(angle, rx, ry);
+      s.scale(sx, sy);
+      if(rf_x) s.reflect(1);
+      if(rf_y) s.reflect(2);
+      if(rf_p) s.reflect(3);
+      s.print();
+      shape_array.push(s);
+      break;
+    case 3:
+      var prism_width = parseInt(document.getElementById('prism_rct_width').value);
+      var prism_height = parseInt(document.getElementById('prism_rct_height').value);
+      var prism_depth = parseInt(document.getElementById('prism_rct_depth').value);
+      s.prismRect(x1, y1, prism_width, prism_height, prism_depth);
+      s.translate(tx, ty);
+      s.rotate(angle, rx, ry);
+      s.scale(sx, sy);
+      if(rf_x) s.reflect(1);
+      if(rf_y) s.reflect(2);
+      if(rf_p) s.reflect(3);
+      s.print();
+      shape_array.push(s);
+      break;
+    case 4:
+      var prism_side = parseInt(document.getElementById('prism_tr_side').value);
+      var prism_depth = parseInt(document.getElementById('prism_tr_depth').value);
+      s.prismTriang(x1, y1, prism_side, prism_depth);
+      s.translate(tx, ty);
+      s.rotate(angle, rx, ry);
+      s.scale(sx, sy);
+      if(rf_x) s.reflect(1);
+      if(rf_y) s.reflect(2);
+      if(rf_p) s.reflect(3);
+      s.print();
+      shape_array.push(s);
+      break;
+      default:
+      break;
   }
 }
 
@@ -74,7 +154,7 @@ class Shape {
     this.coords = [];
   }
   addCoord(x, y) {
-    this.coords.push([x+(w_size/2), y+(w_size/2)]);
+    this.coords.push([x+(w_width/2), y+(w_height/2)]);
   }
   print() {
     for(var i = 0; i < this.coords.length; i++) {
@@ -90,6 +170,7 @@ class Shape {
 
   rotate(angle, xr, yr) {
     var x_prev, y_prev;
+    angle = angle * Math.PI/180;
     // Rotate relative to origin
     var origin_x = this.coords[0][0];
     var origin_y = this.coords[0][1];
@@ -99,7 +180,7 @@ class Shape {
       x_prev = this.coords[i][0];
       y_prev = this.coords[i][1];
       this.coords[i][0] = xr + ((x_prev-xr)*Math.cos(angle))-((y_prev-yr)*Math.sin(angle));
-      this.coords[i][1] = xr + ((x_prev-xr)*Math.sin(angle))+((y_prev-yr)*Math.cos(angle));
+      this.coords[i][1] = yr + ((x_prev-xr)*Math.sin(angle))+((y_prev-yr)*Math.cos(angle));
     }
   }
 
@@ -115,14 +196,14 @@ class Shape {
     // 1 -> reflect on y
     // 2 -> reflect on xy
     for(var i = 0; i < this.coords.length; i++) {
-      if(direction == 0) {
-        this.coords[i][1] -= (w_size/2);
+      if(direction == 1 || direction == 3) {
+        this.coords[i][1] -= (w_height/2);
         this.coords[i][1] *= -1;
-        this.coords[i][1] += (w_size/2);
-      } else if (direction == 1) {
-        this.coords[i][0] -= (w_size/2);
+        this.coords[i][1] += (w_height/2);
+      } else if (direction == 2 || direction == 3) {
+        this.coords[i][0] -= (w_width/2);
         this.coords[i][0] *= -1;
-        this.coords[i][0] += (w_size/2);
+        this.coords[i][0] += (w_width/2);
       } else if (direction == 2) {
         return;
       }
@@ -161,12 +242,79 @@ class Shape {
       inverted *= -1;
     }
   }
+
+  cube(length, x1, y1) {
+    var separation = length / 2;
+    this.polygon(4, length, x1, y1);
+    this.polygon(4, length, x1 + separation, y1 + separation);
+    this.line(x1, y1, x1 + separation, y1 + separation);
+    this.line(x1 + length, y1, x1 + separation + length, y1 + separation);
+    this.line(x1, y1 + length, x1 + separation, y1 + separation + length);
+    this.line(x1 + length, y1 + length, x1 + separation + length, y1 + separation + length);
+  }
+
+  prismRect(x1, y1, width, height, depth) {
+    var separation = depth / 2;
+    this.line(x1, y1, x1 + width, y1);
+    this.line(x1 + width, y1, x1 + width, y1 + height);
+    this.line(x1 + width, y1 + height, x1, y1 + height);
+    this.line(x1, y1 + height, x1, y1);
+    this.line(x1 + separation, y1 + separation, x1 + width + separation, y1 + separation);
+    this.line(x1 + width + separation, y1 + separation, x1 + width + separation, y1 + height + separation);
+    this.line(x1 + width + separation, y1 + height + separation, x1 + separation, y1 + height + separation);
+    this.line(x1 + separation, y1 + height + separation, x1 + separation, y1 + separation);
+    this.line(x1, y1, x1 + separation, y1 + separation);
+    this.line(x1 + width, y1, x1 + separation + width, y1 + separation);
+    this.line(x1, y1 + height, x1 + separation, y1 + separation + height);
+    this.line(x1 + width, y1 + height, x1 + separation + width, y1 + separation + height);
+  }
+
+  prismTriang(x1, y1, length, depth) {
+    var separation = depth / 2;
+    var height = length * Math.sin(Math.PI / 3);
+    this.polygon(3, length, x1, y1);
+    this.polygon(3, length, x1 + separation, y1 + separation);
+    this.line(x1, y1, x1 + separation, y1 + separation);
+    this.line(x1 + length, y1, x1 + separation + length, y1 + separation);
+    this.line(x1 + length/2, y1 + height, x1 + length/2 + separation, y1 + height + separation)
+  }
+
+  circle(x0, y0, radius) {
+    var x = radius-1;
+    var y = 0;
+    var dx = 1;
+    var dy = 1;
+    var err = dx - (radius << 1);
+
+    while (x >= y)
+    {
+        this.addCoord(x0 + x, y0 + y);
+        this.addCoord(x0 + y, y0 + x);
+        // this.addCoord(x0 - y, y0 + x);
+        // this.addCoord(x0 - x, y0 + y);
+        // this.addCoord(x0 - x, y0 - y);
+        // this.addCoord(x0 - y, y0 - x);
+        // this.addCoord(x0 + y, y0 - x);
+        // this.addCoord(x0 + x, y0 - y);
+
+        if (err <= 0) {
+            y++;
+            err += dy;
+            dy += 2;
+        }
+        if (err > 0) {
+            x--;
+            dx += 2;
+            err += (-radius << 1) + dx;
+        }
+    }
+  }
 }
 
 function drawAxis() {
   stroke(0, 153, 255);
-  line(0, w_size*0.5, w_size, w_size*0.5);
-  line(w_size*0.5, 0, w_size*0.5, w_size);
+  line(0, w_height*0.5, w_width, w_height*0.5);
+  line(w_width*0.5, 0, w_width*0.5, w_height);
   stroke(0,0,0);
 }
 
@@ -179,15 +327,15 @@ function setListener() {
 function getCoords() {
   var x = event.clientX;     // Get the horizontal coordinate
   var y = event.clientY;     // Get the vertical coordinate
-  if(x < w_size && y < w_size) {
+  if(x < w_width && y < w_height) {
     var t_space = document.getElementById("t_space");
     var tooltipSpan = document.getElementById('tooltip-span');
     t_space.removeEventListener("click", getCoords);
     t_space.removeEventListener("mousemove", printCoords);
     t_space.style.cursor = "default";
     tooltipSpan.innerHTML = "";
-    curr_coords[0] = x - 16 - w_size/2;
-    curr_coords[1] = y - w_size/2;
+    curr_coords[0] = x - 16 - w_width/2;
+    curr_coords[1] = y - w_height/2;
   }
 }
 
@@ -198,6 +346,11 @@ function printCoords() {
   var y = event.clientY;
   tooltipSpan.style.top = (y + 20) + 'px';
   tooltipSpan.style.left = (x + 20) + 'px';
-  tooltipSpan.innerHTML = Math.round(x - 16 - w_size/2) + ', ' + Math.round(y - w_size/2);
-  coords_text.innerHTML = Math.round(x - 16 - w_size/2) + ', ' + Math.round(y - w_size/2);
+  tooltipSpan.innerHTML = Math.round(x - 16 - w_width/2) + ', ' + Math.round(y - w_height/2);
+  coords_text.innerHTML = Math.round(x - 16 - w_width/2) + ', ' + Math.round(y - w_height/2);
+}
+
+function clearScreen() {
+  clear();
+  drawAxis();
 }
